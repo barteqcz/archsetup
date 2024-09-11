de3086e6d1de2bc52ce302b6b9e823de7383a792
Bartek <118764028+barteqcz@users.noreply.github.com>
2024-09-11 23:30:09 +0200

    Update albi.sh

--- albi.sh
+++ albi.sh
@@ -602,23 +602,11 @@ if [[ "$install_cups" == yes ]]; then
     rm -f /usr/share/applications/hp-uiscan.desktop
 fi
 
-## Install additional AUR packages and run user-specific actions
+## Run user-specific actions
 touch tmpscript.sh
 cat <<'EOY' > tmpscript.sh
 source /config.conf
 cd
-if [[ "$de" == "xfce" ]]; then
-    git clone https://aur.archlinux.org/mugshot.git
-    cd mugshot/
-    makepkg -si --noconfirm
-    cd
-fi
-if [[ "$de" == "cinnamon" ]]; then
-    git clone https://aur.archlinux.org/lightdm-settings.git
-    cd lightdm-settings/
-    makepkg -si --noconfirm
-    cd
-fi
 if [[ "$audio_server" == "pipewire" ]]; then
     systemctl --user enable pipewire pipewire-pulse wireplumber
 elif [[ "$audio_server" == "pulseaudio" ]]; then
@@ -629,14 +617,6 @@ chown "$username":"$username" tmpscript.sh
 echo "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/tmp
 sudo -u "$username" bash tmpscript.sh
 rm -f /etc/sudoers.d/tmp
-
-## Remove PKGBUILDs from AUR
-if [[ "$de" == "xfce" ]]; then
-    rm -rf /home/$username/mugshot
-fi
-if [[ "$de" == "cinnamon" ]]; then
-    rm -rf /home/$username/lightdm-settings
-fi
 
 ## Add sudo privileges for the user
 sed -i '/%wheel ALL=(ALL:ALL) ALL/s/^#//g' /etc/sudoers
